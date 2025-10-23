4b12ed33bb1763f235714c5888f2cccc4125c360
Samuel <samuelshin100@gmail.com>
2025-10-23 17:46:54 -0500

    Update problem4.py

---
+++ problem4.py
@@ -0,0 +1,13 @@
+def fizzbuzz(n):
+  if n % 3 == 0 and n % 5 == 0:
+    return "FizzBuzz"
+  elif n % 3 == 0:
+    return "Fizz"
+  elif n % 5 == 0:
+    return "Bizz"
+  else:
+    return str(n)
+
+print(fizzbuzz(15))
+
+  
